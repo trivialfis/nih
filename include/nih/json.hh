65558603b28b987f932d751d5ca7d8df594421fb
fis <jm.yuan@outlook.com>
2019-03-02 23:16:57 +0800

    Add license.

--- include/nih/json.hh
+++ include/nih/json.hh
@@ -1,3 +1,20 @@
+/* This file is part of NIH.
+ *
+ * Copyright (c) 2019 Jiaming Yuan <jm.yuan@outlook.com>
+ *
+ * NIH is free software: you can redistribute it and/or modify it under the
+ * terms of the Lesser GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * NIH is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the Lesser GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with NIH.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef _JSON_HH_
 #define _JSON_HH_
 
