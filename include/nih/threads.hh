d467eec238d1139fb9909c31e8dcc31ae002df4b
fis <jm.yuan@outlook.com>
2019-05-28 08:30:04 +0800

    Add thread store.

---
+++ include/nih/threads.hh
@@ -0,0 +1,28 @@
+#ifndef _THREADS_HH_
+#define _THREADS_HH_
+
+#include <thread>
+#include <unordered_map>
+
+namespace nih {
+
+template <typename Type>
+class ThreadStore {
+  std::unordered_map<std::thread::id, Type> _store;
+
+ public:
+  Type& getCurrentThread() {
+    return _store.at(std::this_thread::get_id());
+  }
+  Type const& getCurrentThread() const {
+    return _store.at(std::this_thread::get_id());
+  }
+
+  void setCurrentThread(Type&& value) {
+    _store[std::this_thread::get_id()] = std::forward<Type>(value);
+  }
+};
+
+}  // namespace nih
+
+#endif  // _THREADS_HH_
