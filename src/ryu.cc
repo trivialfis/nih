0e37deef30a754f4d533e080df9994c9cfe03771
fis <jm.yuan@outlook.com>
2019-11-17 19:17:39 +0800

    Further refactoring.

--- src/ryu.cc
+++ src/ryu.cc
@@ -1,6 +1,6 @@
 #include <nih/ryu.hh>
 
 namespace nih {
-uint64_t constexpr PowerBaseComputer::kFloatPow5InvSplit[31];
-uint64_t constexpr PowerBaseComputer::kFloatPow5Split[47];
+uint64_t constexpr RyuPowLogUtils::kFloatPow5InvSplit[31];
+uint64_t constexpr RyuPowLogUtils::kFloatPow5Split[47];
 }  // namespace nih
