c7b2dae73a4f8fd8a4f5e49342a84390cbfe62ab
fis <jm.yuan@outlook.com>
2019-11-17 19:51:37 +0800

    Get fpconv in.

--- include/nih/json_writer_experimental.hh
+++ include/nih/json_writer_experimental.hh
@@ -22,49 +22,44 @@ template <> struct NumericLimits<float> {
 };
 
 class JsonWriter {
-  std::vector<std::string::value_type> _buffer;
-  char f2s_buffer[NumericLimits<float>::kDigit10];
-  char i2s_buffer[NumericLimits<int64_t>::kDigit10];
+  std::vector<std::string::value_type> buffer_;
+  char f2s_buffer_[NumericLimits<float>::kDigit10];
+  char i2s_buffer_[NumericLimits<int64_t>::kDigit10];
 
  public:
   void HandleNull() {
-    auto s = _buffer.size();
-    _buffer.resize(s + 4);
-    _buffer[s + 0] = 'n';
-    _buffer[s + 1] = 'u';
-    _buffer[s + 2] = 'l';
-    _buffer[s + 3] = 'l';
+    auto s = buffer_.size();
+    buffer_.resize(s + 4);
+    buffer_[s + 0] = 'n';
+    buffer_[s + 1] = 'u';
+    buffer_[s + 2] = 'l';
+    buffer_[s + 3] = 'l';
   }
   void HandleTrue() {
-    auto s = _buffer.size();
-    _buffer.resize(s + 4);
-    _buffer[s + 0] = 't';
-    _buffer[s + 1] = 'r';
-    _buffer[s + 2] = 'u';
-    _buffer[s + 3] = 'e';
+    auto s = buffer_.size();
+    buffer_.resize(s + 4);
+    buffer_[s + 0] = 't';
+    buffer_[s + 1] = 'r';
+    buffer_[s + 2] = 'u';
+    buffer_[s + 3] = 'e';
   }
   void HandleFalse() {
-    auto s = _buffer.size();
-    _buffer.resize(s + 5);
-    _buffer[s + 0] = 'f';
-    _buffer[s + 1] = 'a';
-    _buffer[s + 2] = 'l';
-    _buffer[s + 3] = 's';
-    _buffer[s + 4] = 'e';
-  }
-  void visit_object() {
-    auto s = _buffer.size();
-    _buffer.resize(s + 5);  // at least 5 bytes
-    _buffer[s] = '{';
+    auto s = buffer_.size();
+    buffer_.resize(s + 5);
+    buffer_[s + 0] = 'f';
+    buffer_[s + 1] = 'a';
+    buffer_[s + 2] = 'l';
+    buffer_[s + 3] = 's';
+    buffer_[s + 4] = 'e';
   }
 
-  void BeginObject() { _buffer.emplace_back('{'); }
-  void EndObject() { _buffer.emplace_back('}'); }
-  void KeyValue() { _buffer.emplace_back(':'); }
-  void Comma() { _buffer.emplace_back(','); }
+  void BeginObject() { buffer_.emplace_back('{'); }
+  void EndObject() { buffer_.emplace_back('}'); }
+  void KeyValue() { buffer_.emplace_back(':'); }
+  void Comma() { buffer_.emplace_back(','); }
 
-  void BeginArray() { _buffer.emplace_back('['); }
-  void EndArray() { _buffer.emplace_back(']'); }
+  void BeginArray() { buffer_.emplace_back('['); }
+  void EndArray() { buffer_.emplace_back(']'); }
 
 
   void HandleString(ConstStringRef string) {
@@ -103,46 +98,45 @@ class JsonWriter {
     }
     buffer += '"';
 
-    auto s = _buffer.size();
-    _buffer.resize(s + buffer.size());
-    std::memcpy(_buffer.data() + s, buffer.data(), buffer.size());
+    auto s = buffer_.size();
+    buffer_.resize(s + buffer.size());
+    std::memcpy(buffer_.data() + s, buffer.data(), buffer.size());
   }
   void HandleFloat(float f) {
     // {
     //   char buffer[24];
     //   size_t len = fpconv_dtoa(f, buffer);
-    //   // LOG_VAR(len);
     //   auto ori_size = _buffer.size();
     //   _buffer.resize(_buffer.size() + len);
     //   std::memcpy(_buffer.data() + ori_size, buffer, len);
     // }
     {
-      auto ret = to_chars(f2s_buffer, f2s_buffer + NumericLimits<float>::kDigit10, f);
+      auto ret = to_chars(f2s_buffer_, f2s_buffer_ + NumericLimits<float>::kDigit10, f);
       auto end = ret.ptr;
       NIH_ASSERT(ret.ec == std::errc());
-      auto out_size = end - f2s_buffer;
-      auto ori_size = _buffer.size();
-      _buffer.resize(_buffer.size() + out_size);
-      std::memcpy(_buffer.data() + ori_size, f2s_buffer, end - f2s_buffer);
+      auto out_size = end - f2s_buffer_;
+      auto ori_size = buffer_.size();
+      buffer_.resize(buffer_.size() + out_size);
+      std::memcpy(buffer_.data() + ori_size, f2s_buffer_, end - f2s_buffer_);
     }
   }
   void HandleInteger(int64_t i) {
-    auto ret = to_chars(i2s_buffer, i2s_buffer + NumericLimits<int64_t>::kDigit10, i);
+    auto ret = to_chars(i2s_buffer_, i2s_buffer_ + NumericLimits<int64_t>::kDigit10, i);
     auto end = ret.ptr;
     NIH_ASSERT(ret.ec == std::errc());
-    auto digits = std::distance(i2s_buffer, end);
-    auto ori_size = _buffer.size();
-    _buffer.resize(ori_size + digits);
-    std::memcpy(_buffer.data() + ori_size, i2s_buffer, digits);
+    auto digits = std::distance(i2s_buffer_, end);
+    auto ori_size = buffer_.size();
+    buffer_.resize(ori_size + digits);
+    std::memcpy(buffer_.data() + ori_size, i2s_buffer_, digits);
   }
 
   std::vector<std::string::value_type> const& write(Value const& value) {
-    return _buffer;
+    return buffer_;
   }
 
   void TakeResult(std::string *str) {
-    str->resize(_buffer.size());
-    std::copy(_buffer.cbegin(), _buffer.cend(), str->begin());
+    str->resize(buffer_.size());
+    std::copy(buffer_.cbegin(), buffer_.cend(), str->begin());
   }
 };
 
